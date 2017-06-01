2353327e49cd7fae4c29b7100213b7d199b94ac2
Harsh Chiki <harshchiki@gmail.com>
2017-06-02 08:35:05 +1100

    KMP algo

---
+++ src/main/java/string/KMPAlgo.java
@@ -0,0 +1,30 @@
+package string;
+
+public class KMPAlgo {
+	public static void main(String[] args) {
+		findMatches("hello world".toCharArray(), "world".toCharArray());
+	}
+	
+	static void findMatches(char[] t, char[] p){
+		int currentIndex = 0;
+		int leftOverChars = t.length;
+		
+		while(leftOverChars > 0 ){
+			for(int i = 1;i<=p.length;i++){
+				if(!(t[currentIndex+i-1] == p[i-1])){
+					currentIndex += i;
+					leftOverChars = t.length - currentIndex;
+					break;
+				} else{
+					if(i == (p.length)){
+						System.out.println("Found at "+currentIndex);
+						
+						leftOverChars = t.length - currentIndex - p.length;
+						currentIndex += i;
+						
+					}
+				}
+			}
+		}
+	}
+}
