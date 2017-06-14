d02804e4cbadbba466b20b6c2c6c73b4f46b8e6a
Harsh Chiki <harshchiki@users.noreply.github.com>
2017-06-14 13:58:00 +0530

    Create CoinChangeProblem.java

--- src/main/java/dynamicprogramming/CoinChangeProblem.java
+++ src/main/java/dynamicprogramming/CoinChangeProblem.java
@@ -3,7 +3,7 @@ package dynamicprogramming;
  * Given a value N, if we want to make change for N cents, 
  * and we have infinite supply of each of S = { S1, S2, .. , Sm}
  *  valued coins, how many ways can we make the change? The order 
- *  of coins doesn’t matter. 
+ *  of coins doesn’t matter. (test commit)
  */
 
 import java.util.Arrays;
