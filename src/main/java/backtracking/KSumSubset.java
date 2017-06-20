934b8ca6b5bf18b412a1828bcb1d45e386015df2
Harsh Chiki <harshchiki@gmail.com>
2017-06-20 09:05:16 +0530

    more changes

--- src/main/java/backtracking/KSumSubset.java
+++ src/main/java/backtracking/KSumSubset.java
@@ -1,5 +1,7 @@
 package backtracking;
 
+import java.util.Stack;
+
 public class KSumSubset {
 
 	public static void main(String[] args) {
@@ -7,17 +9,17 @@ public class KSumSubset {
 	}
 	void init(){
 		int[] a = new int[]{1,2,3};
-		int n = 3;
+		int k = 3;
 		Node root = new Node();
 		root.sum = 0;
 		root.index = 0;
 		root.parent = null;
 		
-		buildTree(a, root, 0);
+		buildTree(a, root, 0, k);
 		System.out.println("Built tree");
 	}
 	
-	void buildTree(int[] a, Node root, int index){
+	void buildTree(int[] a, Node root, int index, int k){
 		if(index == a.length){
 			return;
 		}
@@ -26,14 +28,34 @@ public class KSumSubset {
 		leftNode.parent = root;
 		leftNode.index = root.index + 1;
 		root.left = leftNode;
-		buildTree(a, root.left, index+1);
+		buildTree(a, root.left, index+1, k);
 		
 		Node rightNode = new Node();
 		rightNode.sum = root.sum + a[root.index];
+		if((root.sum+a[root.index]) == k){
+			System.out.println("Found at "+root.index);
+			printTraceForSumSubset(root, a);
+		}
 		rightNode.parent = root;
 		rightNode.index = root.index + 1;
 		root.right = rightNode;
-		buildTree(a, root.right, index+1);
+		buildTree(a, root.right, index+1, k);
+	}
+	
+	void printTraceForSumSubset(Node root, int[] a){
+		Stack<Node> stack = new Stack<>();
+		stack.add(root);
+		
+		Node node = root.parent;
+		while(node != null && node.sum>0){
+			stack.push(node);
+			node = node.parent;
+		}
+		
+		while(!stack.isEmpty()){
+			System.out.print(a[stack.pop().index]+" ");
+		}
+		System.out.println("Printed");
 	}
 	
 	class Node{
