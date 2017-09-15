73f23e49c915ecac987088eed06d497082fae458
Harsh <harshchiki@gmail.com>
2017-09-15 21:18:39 +0530

    Check if a number is a palindrome or not

---
+++ src/main/java/otherproblems/CheckIfANumberIsAAPalindrome.java
@@ -0,0 +1,43 @@
+package otherproblems;
+
+public class CheckIfANumberIsAAPalindrome {
+	public static void main(String[] args) {
+		CheckIfANumberIsAAPalindrome c = new CheckIfANumberIsAAPalindrome();
+		int number = 12321;
+		int divisor = c.getDivisor(number);
+		System.out.println("Divisor = "+divisor);
+		System.out.println(c.isPalindrome(number, divisor));
+	}
+	
+	// Time: O(n), Space: O(1)
+	int getDivisor(int n) {
+		int length = 1;
+		while(n >9) {
+			n /= 10;
+			length++;
+		}
+		
+		int divisor = 1;
+		for(int i = 1;i<length;i++) {
+			divisor *= 10;
+		}
+		return divisor;
+	}
+	
+	// Time: O(n), Space: O(1)
+	boolean isPalindrome(int num, int divisor) {
+		if(num < 10) {
+			return true;
+		}
+		
+		int first = num/divisor;
+		int last = num % 10;
+		
+		if(first != last) {
+			return false;
+		}
+		
+		return isPalindrome(first % divisor,
+				divisor / 10);
+	}
+}
