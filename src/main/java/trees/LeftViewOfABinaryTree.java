1f37cbe5f4151584be0fa4afd7c26fb9a4ba08e9
Harsh <harshchiki@gmail.com>
2017-09-15 08:37:36 +0530

    left view of a binary tree without extra space

---
+++ src/main/java/trees/LeftViewOfABinaryTree.java
@@ -0,0 +1,43 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * Problem: Left view of a binary tree
+ * Time: O(n)
+ * Space: O(1)
+ * 
+ * With the approach using extra space, level order traversal 
+ * could be used to print the first node of each level
+ */
+public class LeftViewOfABinaryTree {
+	int maxDepthExplored = -1;
+	List<TreeNode> leftView = new LinkedList<>();
+	
+	void populateLeftView(TreeNode root, int height) {
+		if(root == null) return;
+		
+		if(maxDepthExplored < height) {
+			maxDepthExplored++;
+			leftView.add(root);
+		}
+		
+		populateLeftView(root.left, height+1);
+		populateLeftView(root.right, height+1);
+	}
+	
+	void print() {
+		leftView.stream().forEach(node -> System.out.print(node.data+" "));
+	}
+	
+	public static void main(String[] args) {
+		LeftViewOfABinaryTree l = new LeftViewOfABinaryTree();
+		l.populateLeftView(TreeUtils.getTree(), 0);
+		System.out.println("Printing inorder");
+		TreeUtils.printInOrder(TreeUtils.getTree());
+		System.out.println();
+		System.out.println("Printing left view");
+		l.print();
+	}
+}
