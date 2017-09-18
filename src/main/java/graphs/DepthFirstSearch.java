5f05973298fc3eee3df2be1d9494e455d22c2f78
Harsh <harshchiki@gmail.com>
2017-09-18 09:01:29 +0530

    plain implementation of depth first search

--- src/main/java/graphs/DepthFirstSearch.java
+++ src/main/java/graphs/DepthFirstSearch.java
@@ -1,5 +1,34 @@
 package graphs;
 
-public class DepthFirstSearch {
+import java.util.HashSet;
+import java.util.Set;
 
+public class DepthFirstSearch {
+	final GraphNode start;
+	private final Set<GraphNode> visitedNodes = new HashSet<>();
+	
+	DepthFirstSearch(final GraphNode node){
+		this.start = node;
+	}
+	
+	
+	public void printDepthFirstSearch() {
+		dfsUtil(this.start);
+	}
+	
+	private void dfsUtil(GraphNode node) {
+		if(node == null) return;
+		
+		if(!visitedNodes.contains(node)) {
+			visitedNodes.add(node);
+			System.out.println(node.data);
+			node.adjNodes.stream().forEach(n -> dfsUtil(n));
+		}
+	}
+	
+	public static void main(String[] args) {
+		GraphNode start = GraphUtils.getAcyclicGraph();
+		DepthFirstSearch dfs = new DepthFirstSearch(start);
+		dfs.printDepthFirstSearch();
+	}
 }
