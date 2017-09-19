7715cc1b0ba3de76807d1518d03287cd084e426c
Harsh <harshchiki@gmail.com>
2017-09-19 21:55:36 +0530

    k sum subset has mistakes

--- src/main/java/backtracking/KSumSubset.java
+++ src/main/java/backtracking/KSumSubset.java
@@ -2,38 +2,86 @@ package backtracking;
 
 import java.util.Stack;
 
+/*
+ * http://www.geeksforgeeks.org/backttracking-set-4-subset-sum/
+ * 
+ * THIS ALGO IS WRONG. Needs to be corrected.
+ */
 public class KSumSubset {
 
 	public static void main(String[] args) {
 		new KSumSubset().init();
 	}
+	
 	void init(){
-		int[] a = new int[]{1,2,3};
-		int k = 3;
+		int[] a = new int[]{2,3,4,5};
+		int k = 7;
 		Node root = new Node();
 		root.sum = 0;
 		root.index = 0;
 		root.parent = null;
 		
+//		buildTree1(a, 0, root, k);
+		
 		buildTree(a, root, 0, k);
-		System.out.println("Built tree");
+//		System.out.println("Built tree");
 	}
 	
+	
+	void buildTree1(int[] a, int index, Node parent, int k) {
+		if(index == a.length-1) {
+			return;
+		}
+		
+		Node leftNode = new Node(parent.sum, index, parent);
+		parent.left = leftNode;
+		buildTree1(a, index+1, leftNode, k);
+		
+		Node rightNode = new Node(parent.sum + a[index], index, parent);
+		parent.right = rightNode;
+		if((parent.sum + a[index]) == k) {
+			printPath(parent, a);
+		}
+		buildTree1(a, index+1, rightNode, k);
+	}
+	
+	void printPath(Node node, int[] a) {
+		Stack<Node> s = new Stack<>();
+		Node tmp = node;
+		
+		while(tmp != null) {
+			s.push(tmp);
+			tmp = tmp.parent;
+		}
+		
+		System.out.println();
+		while(!s.isEmpty()) {
+			System.out.print(a[s.pop().index] + " ");
+		}
+		System.out.println();
+	}
+	
+	// starts with a, root (sum: 0, index: 0, parent: null), 0, given k
 	void buildTree(int[] a, Node root, int index, int k){
 		if(index == a.length){
 			return;
 		}
-		Node leftNode = new Node();
-		leftNode.sum = root.sum;
-		leftNode.parent = root;
-		leftNode.index = root.index + 1;
-		root.left = leftNode;
-		buildTree(a, root.left, index+1, k);
 		
+		// at every stage, we have a choice to choose then element, or not choose that element
+		
+		// left node is for when I do not choose a[index]
+		Node leftNode = new Node();
+		leftNode.sum = root.sum; // sum remains the same
+		leftNode.parent = root; // parent changes to root
+		leftNode.index = root.index + 1; // index certainly increments (doesn't vary, whether or not we have chosen the element)
+		root.left = leftNode; // attaching leftNode to the node passed
+		buildTree(a, root.left, index+1, k); // and go on building the left tree
+		
+		// right node is when I choose a[index]
 		Node rightNode = new Node();
-		rightNode.sum = root.sum + a[root.index];
+		rightNode.sum = root.sum + a[index]; // since we have chose this element, we add a[index] to the sum. a[index] is always == a[index]
 		if((root.sum+a[root.index]) == k){
-			System.out.println("Found at "+root.index);
+//			System.out.println("Found at "+root.index);
 			printTraceForSumSubset(root, a);
 		}
 		rightNode.parent = root;
@@ -51,16 +99,22 @@ public class KSumSubset {
 			stack.push(node);
 			node = node.parent;
 		}
-		
 		while(!stack.isEmpty()){
 			System.out.print(a[stack.pop().index]+" ");
 		}
+		System.out.println();
 		System.out.println("Printed");
 	}
 	
 	class Node{
-		int sum = 0; // sum so far, at this node
+		int sum = 0; // sum so far, at this node (as we go down the tree)
 		int index = -1; // at this node, left is a[index] is not chosen, and right is when a[index] is chosen
 		Node parent, left, right;
+		Node(){}
+		Node(int sum, int index, Node parent){
+			this.sum = sum;
+			this.index = index;
+			this.parent = parent;
+		}
 	}
 }
--- src/main/java/dynamicprogramming/CoinChangeProblem.java
+++ src/main/java/dynamicprogramming/CoinChangeProblem.java
@@ -19,8 +19,11 @@ public class CoinChangeProblem {
 		CoinChangeProblem o = new CoinChangeProblem();
 		System.out.println(o.noOfWaysToMakeChangeRecursive(new int[]{1, 2, 3}, 3, 4));
 		System.out.println(o.noOfWaysToMakeChangeDP(new int[]{3,5,10}, 20));
+		System.out.println(o.noOfWaysToMakeChangeDPAppraoch2(new int[]{3,5,10}, 20));
 	}
 
+	// coins array -> variety (by their value) of coins(DENOMINATIONS). number of coins available is infinite.
+	// m is the index of last coin
 	int noOfWaysToMakeChangeRecursive(int[] coins, int m, int totalValue){
 		if(totalValue == 0){
 			return 1; // only way is to give nothing
@@ -64,5 +67,48 @@ public class CoinChangeProblem {
 		}
 		
 		return table[value];
+	}
+	
+	/*
+	 * Reference: https://www.youtube.com/watch?v=Y0ZqKpToTic&feature=youtu.be
+	 */
+	int noOfWaysToMakeChangeDPAppraoch2(int[] coins, int value) {
+		int[][] table = new int[coins.length+1][value+1];
+		
+		for(int i = 0;i<coins.length;i++) {
+			table[i][0] = 0;
+		}
+		
+		for(int j = 0;j<=value;j++) {
+			table[0][j] = 0;
+		}
+		
+		for(int i = 1;i<=coins.length;i++) {
+			for(int j = 1;j<=value;j++) {
+				int coin = coins[i-1];
+				int valueRequired = j;
+				
+				if(valueRequired <= coin) {
+					table[i][j] = table[i-1][j];
+				}else {
+					int valueToLookup = table[i][j-coin];
+					int upperValue = table[i-1][j];
+					table[i][j] = (valueToLookup < upperValue ? valueToLookup : upperValue)+1;
+				}
+			}
+		}
+		System.out.println("#############");
+		printSol(table);
+		System.out.println("#############");
+		return table[coins.length][value];
+	}
+	
+	void printSol(int[][] table) {
+		for(int i = 0;i<table.length;i++) {
+			for(int j = 0;j<table[0].length;j++) {
+				System.out.print(table[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
