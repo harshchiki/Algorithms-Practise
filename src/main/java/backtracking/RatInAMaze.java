d9559be6b957bc27cf6c96c5a4bb520eeaf737f1
Harsh <harshchiki@gmail.com>
2017-09-19 08:29:05 +0530

    Rat in a maze backtracking solution

--- src/main/java/backtracking/RatInAMaze.java
+++ src/main/java/backtracking/RatInAMaze.java
@@ -3,6 +3,8 @@ package backtracking;
 /**
  * http://www.geeksforgeeks.org/backttracking-set-2-rat-in-a-maze/
  * 
+ * Cells marked with 1 can't be reached
+ * 
  * @author harshchiki
  *
  */
@@ -14,5 +16,75 @@ public class RatInAMaze {
 		maze = new int[N][N];
 	}
 	
+	boolean solveMaze() {
+		if(maze == null || maze.length == 0 || maze[0].length == 0) {
+			return true;
+		}
+		
+		if(maze[0][0] == 1) {
+			// if source is marked 1, the rat will never be able to step into the maze (matrix)
+			return false;
+		}
+		
+		if(maze[maze.length-1][maze[0].length-1] == 1) {
+			// if destination is marked 1, it is never possible to reach
+			return false;
+		}
+		
+		int[][] sol = new int[maze.length][maze[0].length];
+		if(solveMazeUtil(maze, 0,0, sol)) {
+			printSol(sol);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	boolean solveMazeUtil(int[][] maze, int x, int y, int[][] sol) {
+		int n = maze.length;
+		int ny = maze[0].length;
+		if(x == (n-1) && y == (ny-1)) {
+			sol[x][y] = 1;
+			return true;
+		}
+		
+		if(isSafe(maze, x,y)) {
+			sol[x][y] = 1;
+			
+			if(solveMazeUtil(maze, x+1, y, sol)) {
+				return true;
+			}
+			
+			if(solveMazeUtil(maze, x, y+1, sol)){
+				return true;
+			}
+			
+			sol[x][y] = 0; // backtracking
+			return false;
+		}
+		
+		return false;
+	}
+	
+	boolean isSafe(int[][] maze, int x, int y) {
+		int n = maze.length;
+		int ny = maze[0].length;
+		
+		if(x<n && x>=0 && y<ny && y>=0 // whether inside the maze matrix or not
+				&&maze[x][y] == 1) { // whether this cell can be reached or not.
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	void printSol(int[][] sol) {
+		for(int i = 0;i<sol.length;i++) {
+			for(int j = 0;j<maze[0].length;j++) {
+				System.out.print(sol[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
 	
 }
