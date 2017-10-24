1d7c1a5d5d0219d8e7aa2f1377e9fce8ef7d5ccd
Harsh Chiki <harsh.chiki@iongroup.com>
2017-10-24 14:45:43 +0530

    added a comment, TBD later to improve search performance in children

--- src/main/java/trees/TrieNode.java
+++ src/main/java/trees/TrieNode.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class TrieNode {
 	public boolean isLeaf;
 	public Character character;
-	public List<TrieNode> children;
+	public List<TrieNode> children; // should be optimized to be a set or something to facilitate O(1) search complexity
 	public TrieNode() {
 		children = new LinkedList<>();
 	}
