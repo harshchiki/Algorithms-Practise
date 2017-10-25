f50734c7374fe170e48ccfa298f484e400bca8e7
Harsh Chiki <harsh.chiki@iongroup.com>
2017-10-25 09:13:00 +0530

    initial cut for Trie

--- src/main/java/trees/TrieNode.java
+++ src/main/java/trees/TrieNode.java
@@ -1,13 +1,37 @@
 package trees;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class TrieNode {
 	public boolean isLeaf;
 	public Character character;
-	public List<TrieNode> children; // should be optimized to be a set or something to facilitate O(1) search complexity
+	public boolean isEndOfWord = false;
+	public Map<Character, TrieNode> children;
+	
 	public TrieNode() {
-		children = new LinkedList<>();
+		children = new HashMap<>();
+	}
+	
+	public TrieNode(char character) {
+		this.character = Character.valueOf(character);
+		children = new HashMap<>();
+	}
+	
+	public TrieNode(char character, final boolean isEndOfWord) {
+		this.character = Character.valueOf(character);
+		children = new HashMap<>();
+		this.isEndOfWord = isEndOfWord;
+	}
+	
+	
+	@Override
+	public boolean equals(Object other) {
+		if(other != null && other instanceof TrieNode){
+			return this.character.equals(((TrieNode) other).character);
+		}
+		
+		return false;
 	}
 }
